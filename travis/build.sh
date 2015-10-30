978e42503eee129df6e5837ea87be316dce3d9cb
Adam Barth <abarth@chromium.org>
2015-10-30 15:25:34 -0700

    Attempt to upload to Google Storage

--- travis/build.sh
+++ travis/build.sh
@@ -4,4 +4,8 @@ set -ex
 PATH=`pwd`/depot_tools:"$PATH"
 cd src
 ./sky/tools/gn --release
-ninja -C out/Release sky_snapshot
+# ninja -C out/Release sky_snapshot
+
+GIT_REVISION = `git rev-parse HEAD`
+$HOME/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ../gcloud_key_file
+$HOME/google-cloud-sdk/bin/gsutil cp LICENSE gs://mojo/travis-test/$GIT_REVISION/foo
--- travis/setup.sh
+++ travis/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+echo $KEY_FILE > gcloud_key_file
+
+CLOUDSDK_CORE_DISABLE_PROMPTS=1
+curl https://sdk.cloud.google.com | bash
+
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 PATH=`pwd`/depot_tools:"$PATH"
 
