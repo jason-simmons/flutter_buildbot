a9ff13777e57f2d387e0721b514f4d664db9b2a4
Adam Barth <abarth@chromium.org>
2015-10-30 15:35:37 -0700

    Export CLOUDSDK_CORE_DISABLE_PROMPTS to actually disable prompts

--- travis/setup.sh
+++ travis/setup.sh
@@ -5,7 +5,7 @@ echo $KEY_FILE== | base64 --decode > gcloud_key_file.json
 
 set -x
 
-CLOUDSDK_CORE_DISABLE_PROMPTS=1
+export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 curl https://sdk.cloud.google.com | bash
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
