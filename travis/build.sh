3ca8ad204871e66cdce644087874d542b042ceea
Adam Barth <abarth@chromium.org>
2015-10-30 16:25:02 -0700

    Try using the mojo_infra bucket

--- travis/build.sh
+++ travis/build.sh
@@ -8,4 +8,4 @@ cd src
 
 GIT_REVISION=`git rev-parse HEAD`
 $HOME/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ../gcloud_key_file.json
-$HOME/google-cloud-sdk/bin/gsutil cp LICENSE gs://mojo/travis-test/$GIT_REVISION/foo
+$HOME/google-cloud-sdk/bin/gsutil cp LICENSE gs://mojo_infra/travis-test/$GIT_REVISION/foo
--- travis/setup.sh
+++ travis/setup.sh
@@ -10,7 +10,10 @@ curl https://sdk.cloud.google.com | bash
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 PATH=`pwd`/depot_tools:"$PATH"
+gclient help
 
-mkdir src
-echo "success" > src/LICENSE
-# gclient sync
+GIT_REVISION=`cat REVISION`
+git clone https://github.com/flutter/engine.git src
+cd src
+git checkout $GIT_REVISION
+#gclient sync
