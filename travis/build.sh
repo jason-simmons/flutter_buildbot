6c57ed846080eb19eea42fb4d12fe36bea52b7df
Adam Barth <abarth@chromium.org>
2015-11-22 01:40:09 -0800

    Build for Android as well

--- travis/build.sh
+++ travis/build.sh
@@ -11,20 +11,48 @@ GIT_REVISION=`git rev-parse HEAD`
 
 $GCLOUD auth activate-service-account --key-file ../gcloud_key_file.json
 
-./sky/tools/gn --release
+if [ $TRAVIS_OS_NAME = "linux" ]; then
+  if [ $BUILD_TARGET = "device" ]; then
+    ./sky/tools/gn --release --android
+    ninja -C out/android_Release apks/SkyShell.apk flutter.mojo
+    STORAGE_BASE_URL=gs://mojo_infra/flutter/$GIT_REVISION/android-arm
 
-if [ $TRAVIS_OS_NAME = "linux" ]
-then
-  ninja -C out/Release
-  STORAGE_BASE_URL=gs://mojo_infra/flutter/$GIT_REVISION/linux-x64
-  zip -j /tmp/artifacts.zip out/Release/icudtl.dat out/Release/sky_shell out/Release/sky_snapshot out/Release/flutter.mojo
-  $GSUTIL cp /tmp/artifacts.zip $STORAGE_BASE_URL/artifacts.zip
+    zip -j /tmp/artifacts.zip \
+      build/android/ant/chromium-debug.keystore \
+      out/android_Release/apks/SkyShell.apk \
+      out/android_Release/flutter.mojo \
+      out/android_Release/gen/sky/shell/shell/classes.dex \
+      out/android_Release/gen/sky/shell/shell/shell/libs/armeabi-v7a/libsky_shell.so \
+      out/android_Release/icudtl.dat
+
+    $GSUTIL cp /tmp/artifacts.zip $STORAGE_BASE_URL/artifacts.zip
+  fi
+
+  if [ $BUILD_TARGET = "host" ]; then
+    ./sky/tools/gn --release
+    ninja -C out/Release
+    STORAGE_BASE_URL=gs://mojo_infra/flutter/$GIT_REVISION/linux-x64
+    zip -j /tmp/artifacts.zip \
+      out/Release/flutter.mojo \
+      out/Release/icudtl.dat \
+      out/Release/sky_shell \
+      out/Release/sky_snapshot
+    $GSUTIL cp /tmp/artifacts.zip $STORAGE_BASE_URL/artifacts.zip
+  fi
 fi
 
 if [ $TRAVIS_OS_NAME = "osx" ]
 then
-  ninja -C out/Release sky_snapshot
-  STORAGE_BASE_URL=gs://mojo_infra/flutter/$GIT_REVISION/darwin-x64
-  zip -j /tmp/artifacts.zip out/Release/sky_snapshot
-  $GSUTIL cp /tmp/artifacts.zip $STORAGE_BASE_URL/artifacts.zip
+  if [ $BUILD_TARGET = "device" ]; then
+    # We don't yet build an iOS artifacts from this repository.
+    exit 0
+  fi
+
+  if [ $BUILD_TARGET = "host" ]; then
+    ./sky/tools/gn --release
+    ninja -C out/Release sky_snapshot
+    STORAGE_BASE_URL=gs://mojo_infra/flutter/$GIT_REVISION/darwin-x64
+    zip -j /tmp/artifacts.zip out/Release/sky_snapshot
+    $GSUTIL cp /tmp/artifacts.zip $STORAGE_BASE_URL/artifacts.zip
+  fi
 fi
--- travis/setup.sh
+++ travis/setup.sh
@@ -5,6 +5,11 @@ echo $KEY_FILE | base64 --decode > gcloud_key_file.json
 
 set -x
 
+if [ $TRAVIS_OS_NAME = "osx" ] && [ $BUILD_TARGET = "device" ]; then
+  # We don't yet build an iOS artifacts from this repository.
+  exit 0
+fi
+
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 curl https://sdk.cloud.google.com | bash
 
@@ -17,3 +22,18 @@ git clone https://github.com/flutter/engine.git src
 cd src
 git checkout $GIT_REVISION
 gclient sync
+
+if [ $BUILD_TARGET = "device" ]; then
+  sudo dpkg --print-foreign-architectures
+  sudo dpkg --add-architecture i386
+  sudo apt-get update
+  sudo apt-get install -y ant openjdk-7-jdk openjdk-7-jre \
+      libncurses5:i386 libstdc++6:i386 libgcc1:i386 zlib1g:i386
+
+  ./tools/android/download_android_tools.py
+
+  # Remove unused toolchains to save space inside the VM.
+  rm -rf third_party/android_tools/ndk/toolchains/mips*
+  rm -rf third_party/android_tools/ndk/toolchains/aarch64*
+  rm -rf third_party/android_tools/ndk/toolchains/x86*
+fi
